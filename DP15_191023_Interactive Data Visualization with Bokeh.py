773763fbe9f015d20d13a9131f416fcceb4cbfeb
wzisgood1 <zhewang.econ@gmail.com>
2019-12-22 09:04:35 +0800

    Update DP15_191023_Interactive Data Visualization with Bokeh.py

--- DP15_191023_Interactive Data Visualization with Bokeh.py
+++ DP15_191023_Interactive Data Visualization with Bokeh.py
@@ -176,3 +176,66 @@ show(p)
 
 # ********** ********** ********** **********
 # https://campus.datacamp.com/courses/interactive-data-visualization-with-bokeh/basic-plotting-with-bokeh?ex=16
+# ********** **********
+# check the object of ColumnDataSource, to read keys and values
+print(type(source))
+print(source)
+print(type(source.data))
+print(source.data.keys())
+print(source.data['Year'])
+
+# Create a figure with the "box_select" tool: p
+p = figure(tools= 'box_select', x_axis_label= 'Year', y_axis_label= 'Time')
+# Add circle glyphs to the figure p with the selected and non-selected properties
+p.circle(x= 'Year', y= 'Time', selection_color= 'red', nonselection_alpha= 0.1, source= source)
+
+# Specify the name of the output file and show the result
+output_file('selection_glyph.html')
+show(p)
+
+
+# ********** ********** ********** **********
+https://campus.datacamp.com/courses/interactive-data-visualization-with-bokeh/basic-plotting-with-bokeh?ex=17
+# ********** **********
+# import the HoverTool
+from bokeh.models import HoverTool
+
+# Add circle glyphs to figure p
+p.circle(x, y, size=10,
+         fill_color='grey', alpha=0.1, line_color=None,
+         hover_fill_color='firebrick', hover_alpha=0.5,
+         hover_line_color='white')
+# Create a HoverTool: hover
+hover = HoverTool(tooltips=None, mode= 'vline')
+# Add the hover tool to the figure p
+p.add_tools(hover)
+# Specify the name of the output file and show the result
+output_file('hover_glyph.html')
+show(p)
+  
+# ********** ********** ********** **********
+https://campus.datacamp.com/courses/interactive-data-visualization-with-bokeh/basic-plotting-with-bokeh?ex=18
+# ********** **********
+#Import CategoricalColorMapper from bokeh.models
+from bokeh.models import CategoricalColorMapper
+
+# Convert df to a ColumnDataSource: source
+source = ColumnDataSource(df)
+# Make a CategoricalColorMapper object: color_mapper
+color_mapper = CategoricalColorMapper(factors=['Europe', 'Asia', 'US'],
+                                      palette=['red', 'green', 'blue'])
+# Add a circle glyph to the figure p
+p.circle('weight', 'mpg', source=source,
+            color=dict(field='origin', transform= color_mapper),
+            legend='origin')
+
+# Specify the name of the output file and show the result
+output_file('colormap.html')
+show(p)
+
+
+
+  
+
+
+
